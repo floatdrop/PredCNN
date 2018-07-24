1abcbff90756c20608f437bbb77935e93d603b5a
Vsevolod Strukchinsky <floatdrop@gmail.com>
2018-07-24 16:41:35 +0500

    Changes in model

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class video_pixel_network_config:
 
     # Encoder config
     encoder_rmb_num = 8
-    encoder_rmb_dilation = False
-    encoder_rmb_dilation_scheme = [1, 2, 4, 8]
+    encoder_rmb_dilation = True
+    encoder_rmb_dilation_scheme = [1, 2, 4, 8, 1, 2, 4, 8]
 
     # Decoder config
     decoder_rmb_num = 12
@@ -18,7 +18,7 @@ class video_pixel_network_config:
     # Training config
     epochs_num = 50000
     iters_per_epoch = 450
-    truncated_steps = 9
+    truncated_steps = 6
     learning_rate = 3 * 1e-4
 
     # Data config
--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from layers import masked_conv2d, BasicConvLSTMCell
 from logger import Logger
 
 
@@ -11,20 +10,7 @@ class VideoPixelNetworkModel:
             self.sequences = tf.placeholder(tf.float32,
                                             shape=[None, config.truncated_steps + 1] + config.input_shape,
                                             name='sequences')
-            self.inference_prev_frame = tf.placeholder(tf.float32,
-                                                       shape=[None] + config.input_shape,
-                                                       name='inference_prev_frame')
-            self.inference_encoder_state = tf.placeholder(tf.float32,
-                                                          shape=[None, config.input_shape[0],
-                                                                 config.input_shape[1], config.conv_lstm_filters],
-                                                          name='inference_encoder_state')
-            self.inference_current_frame = tf.placeholder(tf.float32,
-                                                          shape=[None] + config.input_shape,
-                                                          name='inference_current_frame')
-            self.initial_lstm_state = tf.placeholder(tf.float32,
-                                                     shape=[2, None, config.input_shape[0],
-                                                            config.input_shape[1], config.conv_lstm_filters],
-                                                     name='initial_lstm_state')
+
         self.build_model()
 
     def multiplicative_unit_without_mask(self, h, dilation_rate, scope):
@@ -80,69 +66,6 @@ class VideoPixelNetworkModel:
 
             return mu
 
-    def multiplicative_unit_with_mask(self, h, mask_type, masked_channels, in_channels, out_channels, scope):
-        with tf.variable_scope('multiplicative_unit_with_mask_' + scope):
-            g1 = masked_conv2d(
-                h,
-                in_channels,  # num of channels in input
-                out_channels,
-                3,
-                mask_type,  # A, B or None
-                masked_channels,
-                padding='SAME',
-                activation=tf.sigmoid,
-                kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                name='g1')
-
-            g2 = masked_conv2d(
-                h,
-                in_channels,  # num of channels in input
-                out_channels,
-                3,
-                mask_type,  # A, B or None
-                masked_channels,
-                padding='SAME',
-                activation=tf.sigmoid,
-                kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                name='g2')
-
-            g3 = masked_conv2d(
-                h,
-                in_channels,  # num of channels in input
-                out_channels,
-                3,
-                mask_type,  # A, B or None
-                masked_channels,
-                padding='SAME',
-                activation=tf.sigmoid,
-                kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                name='g3')
-
-            u = masked_conv2d(
-                h,
-                in_channels,  # num of channels in input
-                out_channels,
-                3,
-                mask_type,  # A, B or None
-                masked_channels,
-                padding='SAME',
-                activation=tf.tanh,
-                kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                name='u')
-
-            g2_h = tf.multiply(g2, h)
-            g3_u = tf.multiply(g3, u)
-
-            if mask_type == 'B':
-                g2_h = tf.multiply(g2, h)
-            else:
-                g2_h = g2
-            g3_u = tf.multiply(g3, u)
-
-            mu = tf.multiply(g1, tf.tanh(g2_h + g3_u))
-
-            return mu
-
     def residual_multiplicative_block_without_mask(self, h, dilation_rate, scope):
         with tf.variable_scope('residual_multiplicative_block_without_mask_' + scope):
             h1 = tf.layers.conv2d(
@@ -173,77 +96,6 @@ class VideoPixelNetworkModel:
 
             return rmb
 
-    def residual_multiplicative_block_with_mask(self, h, scope, first_block=False, last_block=False):
-        with tf.variable_scope('residual_multiplicative_block_with_mask_' + scope):
-
-            if first_block:
-                x, h = h
-                h1 = tf.layers.conv2d(
-                    h,
-                    self.config.rmb_c - self.config.input_shape[2],
-                    1,
-                    padding='same',
-                    activation=None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name='h1'
-                )
-                h1 = tf.concat([x, h1], axis=3)
-            else:
-                h1 = tf.layers.conv2d(
-                    h,
-                    self.config.rmb_c,
-                    1,
-                    padding='same',
-                    activation=None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name='h1'
-                )
-
-            if first_block:
-                h2 = self.multiplicative_unit_with_mask(
-                    h1, 'A',
-                    self.config.input_shape[2],
-                    self.config.rmb_c,
-                    self.config.rmb_c,
-                    '1')
-            else:
-                h2 = self.multiplicative_unit_with_mask(
-                    h1, 'B',
-                    self.config.rmb_c,
-                    self.config.rmb_c,
-                    self.config.rmb_c,
-                    '1')
-
-            h3 = self.multiplicative_unit_with_mask(
-                h2, 'B',
-                self.config.rmb_c,
-                self.config.rmb_c,
-                self.config.rmb_c,
-                '2')
-
-            if last_block:
-                rmb = tf.layers.conv2d(
-                    h3,
-                    256,
-                    1,
-                    padding='same',
-                    activation=None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name='h4'
-                )
-            else:
-                h4 = tf.layers.conv2d(
-                    h3,
-                    2 * self.config.rmb_c,
-                    1,
-                    padding='same',
-                    activation=None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name='h4'
-                )
-                rmb = tf.add(h, h4)
-
-            return rmb
 
     def cascade_multiplicative_unit(self, prev_h, curr_h, dilation_rate, scope):
         with tf.variable_scope('cascade_multiplicative_unit_' + scope):
@@ -289,20 +141,6 @@ class VideoPixelNetworkModel:
 
             return x
 
-    def pixel_cnn_decoders(self, h, x):
-        with tf.variable_scope('pixel_cnn_decoders'):
-            h = self.residual_multiplicative_block_without_mask(h, 1, '00')
-
-            h = (x, h)
-            h = self.residual_multiplicative_block_with_mask(h, str(1), first_block=True)
-
-            for i in range(2, self.config.decoder_rmb_num - 1):
-                h = self.residual_multiplicative_block_with_mask(h, str(i))
-
-            h = self.residual_multiplicative_block_with_mask(h, str(self.config.decoder_rmb_num - 1), last_block=True)
-
-            return h
-
     def resolution_preserving_cnn_decoders(self, h, x):
         with tf.variable_scope('resolution_preserving_cnn_decoders'):
             h = self.residual_multiplicative_block_without_mask(h, 1, '00')
@@ -315,48 +153,22 @@ class VideoPixelNetworkModel:
 
             return h
 
-    def conv_lstm(self, x, h):
-        with tf.variable_scope('Conv_LSTM') as scope:
-            convlstm_cell = BasicConvLSTMCell(
-                [self.config.input_shape[0], self.config.input_shape[1]],
-                [3, 3],
-                self.config.conv_lstm_filters,
-                initializer=tf.contrib.layers.xavier_initializer())
-
-            output, state = convlstm_cell(x, h, scope)
-
-            return output, state
-
-    def encoder_template(self, x, lstm_h):
-        encoder_h = self.resolution_preserving_cnn_encoders(x)
-        encoder_h, lstm_h = self.conv_lstm(encoder_h, lstm_h)
-        return encoder_h, lstm_h
-
-    def decoder_template(self, encoder_h, x_):
-        output = self.pixel_cnn_decoders(encoder_h, x_)
-        return output
+    def cmu_template(self, prev_x, curr_x):
+        encoder_h = self.cascade_multiplicative_unit(prev_x, curr_x, 1, 'cmu')
+        return encoder_h
 
     def build_model(self):
-        lstm_state = tf.contrib.rnn.LSTMStateTuple(self.initial_lstm_state[0], self.initial_lstm_state[1])
-        encoder_network_template = tf.make_template('vpn_encoder', self.encoder_template)
-        decoder_network_template = tf.make_template('vpn_decoder', self.decoder_template)
+        encoder_network_template = tf.make_template('vpn_encoder', self.resolution_preserving_cnn_encoders)
+        cmu_network_template = tf.make_template('vpn_cmu', self.cmu_template)
+        decoder_network_template = tf.make_template('vpn_decoder', self.resolution_preserving_cnn_decoders)
 
         with tf.name_scope('training_graph'):
-            net_unwrap = []
-            for i in range(self.config.truncated_steps):
-                encoder_state, lstm_state = encoder_network_template(self.sequences[:, i], lstm_state)
-                step_out = decoder_network_template(encoder_state, self.sequences[:, i + 1])
-                net_unwrap.append(step_out)
+            encoders = [encoder_network_template(self.sequences[:, i]) for i in range(self.config.truncated_steps)]
 
-            self.final_lstm_state = lstm_state
+            while len(encoders) > 1:
+                encoders = [cmu_network_template(first, second) for first, second in zip(encoders, encoders[1:])]
 
-        with tf.name_scope('wrap_out'):
-            net_unwrap = tf.stack(net_unwrap)
-            self.output = tf.transpose(net_unwrap, [1, 0, 2, 3, 4])
-
-            for i in range(self.config.truncated_steps):
-                Logger.summarize_images(tf.expand_dims(tf.cast(tf.arg_max(self.output[:, i], 3), tf.float32), 3),
-                                        'frame_{0}'.format(i), 'vpn', 1)
+            self.output = decoder_network_template(encoders[0])
 
         with tf.name_scope('loss'):
             labels = tf.one_hot(tf.cast(tf.squeeze(self.sequences[:, 1:]), tf.int32),
@@ -365,12 +177,6 @@ class VideoPixelNetworkModel:
                                 dtype=tf.float32)
             self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.output, labels=labels))
             self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
-
-        with tf.name_scope('inference_graph'):
-            lstm_state = tf.contrib.rnn.LSTMStateTuple(self.initial_lstm_state[0], self.initial_lstm_state[1])
-            self.encoder_state, lstm_state = encoder_network_template(self.inference_prev_frame, lstm_state)
-            self.inference_lstm_state = lstm_state
-            self.inference_output = decoder_network_template(self.inference_encoder_state, self.inference_current_frame)
 
         with tf.name_scope('test_frames'):
             self.test_summaries = []
