3971f3be66f70093b626380a2e6e76404b7df4c5
Vsevolod Strukchinsky <floatdrop@gmail.com>
2018-07-24 16:51:47 +0500

    Cleanup

--- trainer.py
+++ trainer.py
@@ -64,8 +64,6 @@ class Trainer:
 
     def train(self):
         Logger.info("Starting training...")
-        initial_lstm_state = np.zeros((2, self.config.batch_size, self.config.input_shape[0],
-                                       self.config.input_shape[1], self.config.conv_lstm_filters))
 
         for epoch in range(self.cur_epoch_tensor.eval(self.sess), self.config.epochs_num):
             losses = []
@@ -75,11 +73,7 @@ class Trainer:
             for itr in range(self.config.iters_per_epoch):
                 warmup_batch, train_batch = self.data_generator.next_batch()
 
-                feed_dict = {self.model.sequences: warmup_batch,
-                             self.model.initial_lstm_state: initial_lstm_state}
-                lstm_state = self.sess.run(self.model.final_lstm_state, feed_dict)
-
-                feed_dict = {self.model.sequences: train_batch, self.model.initial_lstm_state: lstm_state}
+                feed_dict = {self.model.sequences: train_batch}
                 if itr == self.config.iters_per_epoch - 1:
                     loss, _, summaries = self.sess.run([self.model.loss, self.model.optimizer, self.model.summaries],
                                                        feed_dict)
@@ -104,35 +98,6 @@ class Trainer:
     def test(self):
         Logger.info("Starting testing...")
 
-        initial_lstm_state = np.zeros((2, self.config.batch_size, self.config.input_shape[0],
-                                       self.config.input_shape[1], self.config.conv_lstm_filters))
-
-        if self.config.overfitting:
-            warmup_batch, test_batch = self.data_generator.next_batch()
-        else:
-            warmup_batch, test_batch = self.data_generator.test_batch()
-
-        feed_dict = {self.model.sequences: warmup_batch,
-                     self.model.initial_lstm_state: initial_lstm_state}
-        lstm_state = self.sess.run(self.model.final_lstm_state, feed_dict)
-
-        prev_frame = test_batch[:, 0, :, :, :]
-        for frame in range(self.config.truncated_steps):
-            feed_dict = {self.model.inference_prev_frame: prev_frame, self.model.initial_lstm_state: lstm_state}
-            encoder_state, lstm_state = self.sess.run([self.model.encoder_state, self.model.inference_lstm_state],
-                                                      feed_dict)
-
-            current_frame = np.zeros([1] + self.config.input_shape)
-            for i in range(self.config.input_shape[0]):
-                for j in range(self.config.input_shape[1]):
-                    feed_dict = {self.model.inference_encoder_state: encoder_state,
-                                 self.model.inference_current_frame: current_frame}
-                    output, summaries = self.sess.run([self.model.inference_output, self.model.test_summaries[frame]],
-                                                      feed_dict)
-                    self.logger.add_merged_summary(64 * i + j, summaries)
-                    output = np.argmax(output, axis=3)
-                    current_frame[:, i, j, 0] = output[:, i, j].copy()
-
-            prev_frame = current_frame.copy()
+        raise NotImplementedError()
 
         Logger.info("Testing finished")
